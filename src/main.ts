e06afa8c374096eaf5eaad8dcf4a9dc161aaf6ba
Ville Takala <ville.i.takala@gmail.com>
2020-01-27 02:42:42 +0200

    add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+    const options = new DocumentBuilder()
+        .setTitle('Swagger docs')
+        .setDescription('The API')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
